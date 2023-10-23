2596221d0ce90d9c2bd32c067060b7da6fffdfc6
Bipin Gosain <bipin_gosain@epam.com>
2023-10-23 17:26:14 +0530

    changed base url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://fnuuronv50.execute-api.ap-south-1.amazonaws.com/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://fnuuronv50.execute-api.ap-south-1.amazonaws.com/",
+  bff: "https://fnuuronv50.execute-api.ap-south-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
