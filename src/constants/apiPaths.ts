1c25d5599657d95a7519307e9985376198610b0e
Bipin Gosain <bipin_gosain@epam.com>
2023-10-28 10:57:32 +0530

    Completed integration with task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://fnuuronv50.execute-api.ap-south-1.amazonaws.com/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://h7j3jy4wrj.execute-api.ap-south-1.amazonaws.com",
   bff: "https://fnuuronv50.execute-api.ap-south-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
